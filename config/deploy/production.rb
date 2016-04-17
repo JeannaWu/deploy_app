9c45d768dcc0ad17dc6078dfe33d8f2722c4e34f
ZHIYUN WU <wzyunzx@163.com>
2016-04-17 19:29:39 +0100

    add new

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+set :stage, :production
 
+server '139.59.160.58', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
